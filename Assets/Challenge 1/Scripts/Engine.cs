a598d68edb1df5ac32ce501deb2f23bf833da105
Sylvain CAU <ashdevfr@gmail.com>
2021-05-15 11:54:37 -0700

    Init

---
+++ Assets/Challenge 1/Scripts/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    public float rotationSpeed = 1000.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float tiltSpeedScale = rotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.forward * tiltSpeedScale);
+    }
+}
